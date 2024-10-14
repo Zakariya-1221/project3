ee9a3c5b07d6e077a0016b8eb6cf6e3a94b7b8b7
Chen Lin <lin.4096@osu.edu>
2024-10-14 16:10:50 -0400

    added gradebook days and blackout dates to server.rb

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# Gemfile
+source "https://rubygems.org"
+
+gem "dotenv"
+gem "httparty"
+gem "sinatra"
+gem 'sinatra-cross_origin'
+
+
+gem "rackup", "~> 2.1"
---
+++ server.rb
@@ -0,0 +1,135 @@
+require 'sinatra'
+require 'dotenv/load'
+require 'httparty'
+require 'json'
+require 'sinatra/cross_origin'
+
+# Enable cross-origin requests
+configure do
+  enable :cross_origin
+end
+
+# Allow preflight requests (OPTIONS)
+options "*" do
+  response.headers["Allow"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+  200
+end
+
+# Set Access-Control-Allow-Origin header for all responses
+before do
+    response.headers["Access-Control-Allow-Origin"] = "*"
+end
+
+# Load your access token from an environment variable
+ACCESS_TOKEN = ENV['CANVAS_ACCESS_TOKEN']
+BASE_URL = "https://osu.instructure.com/api/v1"
+
+# Fetch courses to identify course_id
+def get_courses
+  puts "Using Access Token: #{ACCESS_TOKEN}"  # Debugging line
+  response = HTTParty.get("#{BASE_URL}/courses", headers: { "Authorization" => "Bearer #{ACCESS_TOKEN}" })
+  if response.code == 200
+    JSON.parse(response.body)
+  else
+    puts 'Courses'
+    puts "Failed to retrieve data: #{response.code}"
+    []
+  end
+end
+
+# Fetch upcoming assignments for a specific course
+def get_upcoming_assignments(course_id)
+  response = HTTParty.get("#{BASE_URL}/courses/#{course_id}/assignments?bucket=upcoming", headers: { "Authorization" => "Bearer #{ACCESS_TOKEN}" })
+  if response.code == 200
+    assignments = JSON.parse(response.body)
+    return assignments
+  else
+    puts "Failed to retrieve data: #{response.code}"
+    []
+  end
+end
+
+
+# Fetch the gradebook for a specific course
+def get_gradebook_day(course_id)
+    response = HTTParty.get("#{BASE_URL}/courses/#{course_id}/gradebook_history/days", headers: { "Authorization" => "Bearer #{ACCESS_TOKEN}" })
+    if response.code == 200
+      gradebook = JSON.parse(response.body)
+      return gradebook
+    else
+      puts "Failed to retrieve data: #{response.code}"
+      []
+    end
+  end
+  
+  # Fetch the blackout_dates for a specific course
+def get_blackout_date(course_id)
+    response = HTTParty.get("#{BASE_URL}/courses/#{course_id}/blackout_dates", headers: { "Authorization" => "Bearer #{ACCESS_TOKEN}" })
+    if response.code == 200
+      blackout_dates = JSON.parse(response.body)
+      return blackout_dates
+    else
+      puts "Failed to retrieve data: #{response.code}"
+      []
+    end
+  end
+  
+
+# Route to serve the assignments data
+get '/assignments' do
+  content_type :json
+  courses = get_courses
+  courses_with_assignments = courses.map do |course|
+    {
+      name: course['name'],
+      assignments: get_upcoming_assignments(course['id'])
+    }
+  end
+  courses_with_assignments.to_json
+end
+
+# Route to serve the gradebook data
+get '/gradebook' do
+content_type :json
+courses = get_courses
+gradebook = courses.map do |course|
+    gradebook_day = get_gradebook_day(course['id'])
+    {
+    course_name: course['name'],
+    days: gradebook_day.map do |day|
+        {
+        date: day['date'],
+        graders: day['graders']
+        }
+    end
+    }
+end
+gradebook.to_json
+end
+
+# Route to serve the blackout_dates data
+get '/blackout_dates' do
+    content_type :json
+    courses = get_courses
+    course_blackout_dates = courses.map do |course|
+        blackout_date = get_blackout_date(course['id'])
+        {
+        name: course['name'],
+        title: blackout_date['event_title'],
+        start_date: blackout_date['start_date'],
+        end_date: blackout_date['end_date']
+        }
+    end
+    course_blackout_dates.to_json
+end
+
+# Serve the HTML page
+get '/' do
+  send_file 'index.html'
+end
+
+# Start the Sinatra application
+set :port, 4567  # Change this to your preferred port
